5666ae8bd676b5a7ae41ed38ec704c97d6a3ac83
Krishnaa-Vinod <kriskun2002@gmail.com>
2025-06-09 14:13:21 -0700

    first commit

---
+++ build/ur5_camera_movit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/krishna/ur_ws/src/ur5_camera_movit_config
+# Build directory: /home/krishna/ur_ws/src/build/ur5_camera_movit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/ur5_camera_movit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/krishna/ur_ws/src/install/ur_sim/share/ur_sim/package.sh"
